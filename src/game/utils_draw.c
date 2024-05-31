afe79887634042fc1891d72c36db539d4527a4f6
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-05-31 14:44:01 +0200

    fix: draw_line()

--- src/game/utils_draw.c
+++ src/game/utils_draw.c
@@ -6,36 +6,11 @@
 /*   By: ohladkov <ohladkov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 19:01:31 by ohladkov          #+#    #+#             */
-/*   Updated: 2024/05/30 19:56:37 by ohladkov         ###   ########.fr       */
+/*   Updated: 2024/05/31 14:43:16 by ohladkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../cub3d.h"
-
-// void	draw_line(t_line *line, t_data *data)
-// {
-// 	int	y;
-// 	int	y_max;
-
-// 	if (line->y0 < line->y1)
-// 	{
-// 		y = line->y0;
-// 		y_max = line->y1;
-// 	}
-// 	else
-// 	{
-// 		y = line->y1;
-// 		y_max = line->y0;
-// 	}
-// 	if (y >= 0)
-// 	{
-// 		while (y < y_max)
-// 		{
-// 			ft_pixel_put(&data->img, line->x0, y, line->color);
-// 			y++;
-// 		}
-// 	}
-// }
 
 void	draw_line(t_line *line, t_data *data)
 {
@@ -43,24 +18,21 @@ void	draw_line(t_line *line, t_data *data)
 	int	y_max;
 
 	// printf("xo: %i, yo:  %i, y1: %i\n", line->x0, line->y0, line->y1);
-	if (line->y0 < line->y1)
+	y = line->y0;
+	y_max = line->y1 + line->y0;
+	if (y < 0)
 	{
-		y = line->y0;
+		y = 0;
 		y_max = line->y1;
 	}
-	else
+	if (line->x0 < 0)
 	{
-		y = line->y1;
-		y_max = line->y0;
+		line->x0 = 0;
+		
 	}
-	if (y < 0)
-		y = 0;
-	if (y >= 0)
+	while (y < y_max)
 	{
-		while (y < y_max)
-		{
-			ft_pixel_put(&data->img, line->x0, y, line->color);
-			y++;
-		}
+		ft_pixel_put(&data->img, line->x0, y, line->color);
+		y++;
 	}
 }
